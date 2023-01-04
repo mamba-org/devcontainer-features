7e2344710caae714c9350c3ced2574d6e6bbd539
eitsupi <ts1s1andn@gmail.com>
2023-01-04 12:35:53 +0900

    move install test to the general test

--- test/micromamba/test.sh
+++ test/micromamba/test.sh
@@ -7,4 +7,17 @@ source dev-container-features-test-lib
 
 check "default (latest) version" micromamba --version
 
+no_conda_forge() {
+    micromamba config list | (! grep -q conda-forge)
+}
+check "no-conda-forge" no_conda_forge
+
+micromamba install --yes --channel=conda-forge python
+
+test_python() {
+    python -c "print(123 + 456)" | grep -q "579"
+}
+check "test-python" test_python
+
+# Report result
 reportResults
