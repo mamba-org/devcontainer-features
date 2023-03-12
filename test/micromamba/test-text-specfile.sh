0949bc4019fe7e6acf29d692c0ffb8cc29238697
Ben Mares <services-git-throwaway1@tensorial.com>
2023-03-12 22:37:24 +0100

    Explain why we manually run the hook in tests

--- test/micromamba/test-text-specfile.sh
+++ test/micromamba/test-text-specfile.sh
@@ -7,6 +7,9 @@ source dev-container-features-test-lib
 
 check "specfile" sh -c "cat /tmp/specfile.txt"
 
+# The test scripts run in a non-interactive Bash shell, so ~/.bashrc is not
+# sourced.  Therefore, we need to manually install the micromamba shell hook.
+# <https://github.com/mamba-org/devcontainer-features/pull/11#issuecomment-1465057342>
 eval "$(micromamba shell hook --shell=bash)"
 
 check "activate" micromamba activate testenv
